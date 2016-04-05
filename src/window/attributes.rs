012510fe24965b4477a70715d004e163da43c5ab
panicbit <panicbit.dev@gmail.com>
2016-04-06 00:11:45 +0200

    Refactor Attributes into submodule

---
+++ src/window/attributes.rs
@@ -0,0 +1,21 @@
+use xcb;
+use super::{MapState, WindowRef};
+
+#[derive(Debug,Eq,PartialEq)]
+pub struct Attributes {
+    pub map_state: MapState
+}
+
+impl Attributes {
+    pub fn from<'a>(window: &WindowRef<'a>) -> Result<Attributes, xcb::GenericError> {
+        let attrs = xcb::get_window_attributes(window.conn.as_xcb(), window.id());
+        let attrs = try!(attrs.get_reply());
+        Ok(Attributes::from_xcb(attrs))
+    }
+
+    fn from_xcb(attrs: xcb::GetWindowAttributesReply) -> Attributes {
+        Attributes {
+            map_state: MapState::from_xcb(attrs.map_state() as u32)
+        }
+    }
+}
